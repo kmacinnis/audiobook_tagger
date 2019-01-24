7f73f53aa0fba34f6572adce1a2e03284ba65571
Kate MacInnis <macinnis.k@gmail.com>
2019-01-24 10:45:01 -0700

    Assorted junk - doesn't work at this point

--- images.py
+++ images.py
@@ -2,19 +2,135 @@
 
 
 
+mimetype = {
+    'JPEG' : 'image/jpeg',
+    'PNG' : 'image/png',
+}
 
 
 
+# fd = urllib.urlopen(cover)
+# # Drop the entire PIL part
+# covr = MP4Cover(fd.read(), getattr(
+#             MP4Cover,
+#             'FORMAT_PNG' if cover.endswith('png') else 'FORMAT_JPEG'
+#         ))
+# fd.close() # always a good thing to do
+#
+# tags['covr'] = [covr] # make sure it's a list
+# tags.save()
 
-fd = urllib.urlopen(cover)
-# Drop the entire PIL part
-covr = MP4Cover(fd.read(), getattr(
-            MP4Cover,
-            'FORMAT_PNG' if cover.endswith('png') else 'FORMAT_JPEG'
-        ))
-fd.close() # always a good thing to do
 
-tags['covr'] = [covr] # make sure it's a list
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, error
+
+audio = MP3(mp3file, ID3=ID3)
+
+
+from PIL import ImageFile
+
+
+p = ImageFile.Parser()
+
+while 1:
+    s = apic.data.read(1024)
+    if not s:
+        break
+    p.feed(s)
+
+im = p.close()
+
+square_image = im.resize((max(im.size),max(im.size)))
+
+import io
+
+with io.BytesIO() as output:
+    square_image.save(output, format=im.format)
+    data = output.getvalue()
+
+
+
+audio.tags.add(
+    APIC(
+        encoding=3,                 # 3 is for utf-8
+        mime=mimetype[im.format],   # image/jpeg or image/png
+        type=3,                     # 3 is for the cover image
+        desc='Cover',
+        data=data
+    )
+)
+audio.save()
+
+
+
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, error, PictureType, Encoding
+
+from PIL import Image
+from PIL import ImageFile
+import io
+
+
+# ipython history:
+
+mimetype = {
+    'JPEG' : 'image/jpeg',
+    'PNG' : 'image/png',
+}
+mp3file = "/Volumes/media/temp-audiobooks/new/Cecilia Grant/A Gentleman Undone/A Gentleman Undone - Part 02.mp3"
+audio = MP3(mp3file, ID3=ID3)
+apic = audio.tags['APIC:']
+parser = ImageFile.Parser()
+parser.feed(apic.data)
+im = parser.close()
+
+square_image = im.resize((max(im.size),max(im.size)))
+with io.BytesIO() as output:
+    square_image.save(output, format=im.format)
+    data = output.getvalue()
+
+
+
+newapic = APIC(
+        encoding=Encoding.UTF8,
+        mime=mimetype[im.format],
+        type=PictureType.COVER_FRONT,
+        desc='Cover',
+        data=data
+    )
+audio.tags.add(newapic)
+audio.save()
+history
+
+mp3file = "/Volumes/media/temp-audiobooks/new/Cecilia Grant/A Gentleman Undone/A Gentleman Undone - Part 03.mp3"
+tags = ID3(mp3file)
+apic = tags['APIC:']
+parser = ImageFile.Parser()
+parser.feed(apic.data)
+im = parser.close()
+square_image = im.resize((max(im.size),max(im.size)))
+with io.BytesIO() as output:
+    square_image.save(output, format=im.format)
+    data = output.getvalue()
+newapic = APIC(
+        encoding=Encoding.UTF8,
+        mime=mimetype[im.format],
+        type=PictureType.COVER_FRONT,
+        desc='Cover',
+        data=data
+    )
+tags.delall('APIC:')
+tags.add(newapic)
 tags.save()
+
+
+
+
+
+
+
+
+
+
 
 
