3a9c775c5442c83c13516f92c2f3fddeec94f53f
Kate MacInnis <macinnis.k@gmail.com>
2019-09-05 10:46:25 -0600

    Add ability to compile image search links

--- images.py
+++ images.py
@@ -12,6 +12,7 @@ mimetype = {
 }
 
 DOC_NAME = '_image_search_links.html'
+COMPILED_DOC_NAME = '_compiled_search_links.html'
 PAGE_START = '''
 <!DOCTYPE html>
 <html>
@@ -25,8 +26,19 @@ PAGE_END = '''
 </body>
 </html> 
 '''
-SEARCH_ITEM = '<p><a href="https://google.com/search?q={0}&tbm=isch">{0}</a></p>'
+SEARCH_ITEM = '<p><a href="https://google.com/search?q={0}&tbm=isch">{0}</a></p>\n'
 
+def sort_search_items(items):
+
+    def key(item):
+        if 'audiobook cover' in item:
+            return '1' + item
+        elif 'author photo' in item:
+            return '2' + item
+        else:
+            return '3' + item
+
+    items.sort(key=key)
 
 def create_image_search_links(search_items, docpath=NEW, docname=DOC_NAME):
     path = unique_path(Path(docpath) / docname)
@@ -36,12 +48,29 @@ def create_image_search_links(search_items, docpath=NEW, docname=DOC_NAME):
             search_doc.write(SEARCH_ITEM.format(item,))
         search_doc.write(PAGE_END)
 
+def compile_image_search_links(docpath=NEW, docname=COMPILED_DOC_NAME):
+    docpath = Path(docpath)
+    image_search_links =[x for x in docpath.iterdir() if x.name.startswith('_image_search_links')]
+    textlines = set()
+    newpath = unique_path(Path(docpath) / docname)
+    with open(newpath, 'w') as compiled_doc:
+        for f in image_search_links:
+            with open(docpath / f, 'r') as search_link_file:
+                newlines = [x for x in list(search_link_file) if x.startswith('<p>')]
+                textlines = textlines.union(newlines)
+        textlines = list(textlines)
+        compiled_doc.write(PAGE_START)
+        sort_search_items(textlines)
+        compiled_doc.writelines(textlines)
+        compiled_doc.write(PAGE_END)
+
 def stretch_cover_image(mp3file):
     tags = ID3(mp3file)
     #TODO: Decide whether should do anything with mulitple pictures
     for tag in tags.keys():
         if tag[:4] == "APIC":
             apic_tag = tag
+
             break
     try:
         apic = tags[apic_tag]
