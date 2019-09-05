aa2e33546aeba2ae340a1bca4e7ef05199dd0c18
Kate MacInnis <macinnis.k@gmail.com>
2019-09-05 10:44:35 -0600

    Clear 'needs photo' notes

--- organize.py
+++ organize.py
@@ -4,7 +4,7 @@ import mutagen
 from enum import Enum, auto
 from pathlib import Path
 
-from common import MAIN, NEW, KIDS_CHAPTERBOOKS
+from common import MAIN, NEW, KIDS_CHAPTERBOOKS, makelist, get_single_mp3
 
 
 class OrgType(Enum):
@@ -43,16 +43,28 @@ def organize_by_tag(root, orgtype=OrgType.AUTHOR_BOOK):
         newpathandname = root / relpath
         os.renames(oldpathandname, newpathandname)
 
+def clear_notes(directory=NEW):
+    directory = Path(directory)
+    for author_dir in (x for x in directory.iterdir() if x.is_dir()):
+        contents = [x.stem for x in author_dir.iterdir()]
+        if '_ needs photo' in contents and 'artist' in contents:
+            unneeded = author_dir / '_ needs photo'
+            os.remove(unneeded)
+
 def merge(to_dir=MAIN, from_dir=NEW, overwrite=False):
     print(f"Merging files from {from_dir} to {to_dir}")
     for root, dirs, files in os.walk(from_dir, followlinks=True):
         for name in files:
+            if name == '.DS_Store':
+                continue
             oldpathandname = os.path.join(root, name)
             relpath = os.path.relpath(oldpathandname, start=from_dir)
             newpathandname = os.path.join(to_dir,relpath)
             if overwrite or not os.path.exists(newpathandname):
                 os.renames(oldpathandname, newpathandname)
                 print(f" - {relpath}")
+    for item in os.listdir(from_dir):
+        pass
 
 def get_parent_folders(filelist):
     parent_folders = set()
@@ -60,7 +72,7 @@ def get_parent_folders(filelist):
         parent_folders.add(os.path.dirname(f))
 
 def move_to_final_destination(dirs=None, booklist=None):
-    if booklist is None:                 
+    if booklist is None:
         if dirs is None:
             dirs = NEW
         books = makelist(dirs)
@@ -87,7 +99,7 @@ def move_to_final_destination(dirs=None, booklist=None):
             continue
         shutil.move(book, new_dir)
         print(f"* {title} moved to {final_dir}")
-    
+
 def pull_contents_from_folder(directory, prefix_with_folder=False):
     print("«Pulling all files into parent folder»")
     if prefix_with_folder:
@@ -112,10 +124,10 @@ def pull_contents_from_folder(directory, prefix_with_folder=False):
 
 def delete_unwanted_files(directory):
     trashextensionlist = [  '.rar' , '.srr' , '.sfv' , '.nzb' , '.tbn' ,
-                            '.nfo' , '.jpg' , '.gif' , '.png' , '.md5' , 
+                            '.nfo' , '.jpg' , '.gif' , '.png' , '.md5' ,
                             '.txt' , '.url' , '.par2' , '.par', '.srs'
                             '.0', '.1' , '.2' , '.3' , '.4' , '.5' , '.6' , '.7' , '.8' , '.9' ]
-    
+
     for root, dirs, files in os.walk(directory):
         for name in files:
             filepathandname = Path(root) / name
